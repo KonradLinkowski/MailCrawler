df262c6d9008c8272c86f0813d4598a7276d50b7
Konrad Linkowski <linkowski.konrad97@gmail.com>
2019-03-21 22:44:04 +0100

    Real file parsing

--- crawler.js
+++ crawler.js
@@ -2,40 +2,49 @@ require('dotenv').config()
 const fs = require('fs')
 const axios = require('axios')
 const mongoose = require('mongoose')
+const URL = require('url').URL
+const jsdom = require('jsdom').JSDOM
 const Link = require('./schemas/link')
 const Mail = require('./schemas/mail')
 
 class Crawler {
   constructor() {
     this.mailRegex = /[a-z0-9._+-]+(@|\[at\])[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}/gi
-    this.linkRegex = 
+    this.linkRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi
     
     this.hostRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}/gi
     this.noPageRegex = /\.(jpeg|jpg|png|gif|mp4|mp3|wav|css|js|webm|ogg|ico|sv|gz|zip|exe|jar|pdf|ttf)$/gi
-    this.visitedLinks = []
-    this.visitedMails = []
+    this.response = null
     this.data = ''
+    this.url = ''
     this.index = 0
   }
   async search(link) {
-    const response = await axios.get(link, { timeout: 5000, transformResponse: res => res })
-    this.data = response.data
+    this.url = link
+    this.response = await axios.get(link, { timeout: 5000, transformResponse: res => res })
+    this.data = this.response.data
     this.index += 1
   }
   async retrieveLinks() {
-    const retrieved = [... new Set(this.data.match(this.linkRegex))].filter(l => !l.match(this.noPageRegex))
-    const hosts = [... new Set(retrieved.map(e => e.match(this.hostRegex)))]
-    Array.prototype.push.apply(this.visitedLinks, retrieved)
-    Array.prototype.push.apply(this.visitedMails, hosts)
-    const final = (await Promise.all(retrieved.map(async url => {
+    let links = []
+    if (this.response.headers['content-type'].includes('html')) {
+        const anchors = [... new jsdom(this.data).window.document.querySelectorAll('a')].map(e => new URL(e, this.url)).filter(e => e.host.length)
+        const retrieved = [... new Set(anchors.map(e => e.href))].filter(l => !l.match(this.noPageRegex))
+        const hosts = [... new Set(retrieved.map(e => e.origin))]
+        links = retrieved.concat(hosts)
+    } else {
+      const retrieved = [... new Set(this.data.match(this.linkRegex))].filter(l => !l.match(this.noPageRegex))
+      const hosts = [... new Set(retrieved.map(e => e.match(this.hostRegex)))]
+      links = retrieved.concat(hosts)
+    }
+    const final = (await Promise.all(links.map(async url => {
       const found = await Link.findOne({ url })
       return found ? null : url
-    }))).filter(e => e)
+    }))).filter(e => e).filter(link => Buffer.from(link).length <= 1024)
     return final
   }
   async retrieveMails() {
     const retrieved = [... new Set(this.data.match(this.mailRegex))].filter(l => !l.match(this.noPageRegex))
-    Array.prototype.push.apply(this.visitedLinks, retrieved)
     const final = (await Promise.all(retrieved.map(async mail => {
       const found = await Mail.findOne({ mail })
       return found ? null : mail
@@ -80,6 +89,9 @@ const start = async (link) => {
     } catch (error) {
       console.log(url)
       console.error(error.message)
+      if (error.response && error.response.status === 429) {
+        linkQueue.length = 0
+      }
       if (['ENOTFOUND'].includes(error.code) || (error.response && error.response.status >= 400)) {
         console.log('deleting:', url)
         await Link.deleteOne({ url })
